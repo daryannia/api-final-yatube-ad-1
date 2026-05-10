7b66522fb9eb2b779eefb5eedf8f056867074108
Shindina Darya <shindina_d@mail.ru>
2026-05-10 12:40:05 +0300

    Apdate paginator in views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -19,6 +19,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = None
 
     def get_queryset(self):
         return Comment.objects.filter(post_id=self.kwargs['post_id'])
@@ -31,6 +32,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    pagination_class = None
 
 
 class FollowViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
@@ -39,6 +41,7 @@ class FollowViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('following__username',)
+    pagination_class = None
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
